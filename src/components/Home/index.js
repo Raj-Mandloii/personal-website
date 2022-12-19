9354b0e4f4b872e0f1e92150dbc7a725cffbe758
Raj Mandloii <rajmandloi01.01@gmail.com>
2022-12-19 17:00:11 +0530

    home page logo animation added

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,6 +3,7 @@ import LogoTitle from "../../assets/images/logo-s.png"
 import React, { useEffect, useState } from 'react'
 import { Link } from "react-router-dom"
 import AnimatedLetters from '../AnimatedLetters/index'
+import Logo from "./Logo/index"
 const Home = () => {
     const [letterClass,setLetterClass] = useState("text-animate")
     const nameArray = "mandloi".split("")
@@ -38,6 +39,7 @@ const Home = () => {
                     <h2>Frontend Developer / JavaSript Developer / Flutter Developer</h2>
                     <Link to="/contact" className="flat-button">CONTACT ME</Link>
             </div>
+            <Logo/>
         </div>
     )
 }
