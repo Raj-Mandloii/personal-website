b687c120048417f1185cc440ea9f0ecfc9abd00d
Raj Mandloii <rajmandloi01.01@gmail.com>
2022-12-19 16:32:27 +0530

    animation added to home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,15 +1,37 @@
 import "./index.scss"
 import LogoTitle from "../../assets/images/logo-s.png"
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from "react-router-dom"
-
+import AnimatedLetters from '../AnimatedLetters/index'
 const Home = () => {
+    const [letterClass,setLetterClass] = useState("text-animate")
+    const nameArray = "mandloi".split("")
+    const jobArray = "web developer.".split("")
+
+    useEffect(()=>{
+         setTimeout(()=>{
+         return   setLetterClass("text-animate-hover")
+        },4000)
+    },[])
     return (
         <div className="container home-page">
             <div className="text-zone">
-                <h1>Hi, <br /> I'm
+                <h1>
+                    <span className={letterClass}>H</span>
+                    <span className={`${letterClass} _12`}>i,</span>
+                    <br /> 
+                    <span className={`${letterClass} _13`}>I</span>
+                    <span className={`${letterClass} _14`}>'m</span>
                     <img src={LogoTitle} alt="developer" />
-                    Mandloi
+                    <AnimatedLetters letterClass={letterClass}
+                    strArray={nameArray}
+                    idx={15}
+                    />
+                    <br/>
+                    <AnimatedLetters letterClass={letterClass}
+                    strArray={jobArray}
+                    idx={22}
+                    />
                     <br />
                     web developer
                     </h1>
