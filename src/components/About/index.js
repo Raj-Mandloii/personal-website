38d819d8c14a3ddb035657ac2cb9975fe5ef9ed9
Raj Mandloii <rajmandloi01.01@gmail.com>
2022-12-20 12:59:07 +0530

    loader added to about and home

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react'
 import AnimatedLetters from '../AnimatedLetters'
 import "./index.scss"
 import { faCss3, faGithub, faHtml5, faJsSquare, faNodeJs, faReact } from "@fortawesome/free-brands-svg-icons"
+import Loader from 'react-loaders'
 
 const About = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
@@ -13,46 +14,50 @@ const About = () => {
         }, 3000)
     }, [])
     return (
-        <div className='container about-page'>
+        <>
 
-            <div className='text-zone'>
-                <h1>
-                    <AnimatedLetters
-                        letterClass={letterClass}
-                        strArray={"About me".split("")}
-                        idx={15} />
-                </h1>
-                <p>My name is Raj and I am a software engineer specialized in front-end and back-end applications.</p>
-                <p>I acquired a strong knowledge and expertise in this field over time. Software quality, scalability and Service Oriented Architecture are subjects that fascinate me. </p>
-                <p>Generally speaking, I love working and sharing on topics related to software architecture and software quality.</p>
-                <p>I like to see myself as a passionate developer, and an eternal learner.</p>
+            <div className='container about-page'>
 
-            </div>
-            <div className='stage-cube-cont'>
+                <div className='text-zone'>
+                    <h1>
+                        <AnimatedLetters
+                            letterClass={letterClass}
+                            strArray={"About me".split("")}
+                            idx={15} />
+                    </h1>
+                    <p>My name is Raj and I am a software engineer specialized in front-end and back-end applications.</p>
+                    <p>I acquired a strong knowledge and expertise in this field over time. Software quality, scalability and Service Oriented Architecture are subjects that fascinate me. </p>
+                    <p>Generally speaking, I love working and sharing on topics related to software architecture and software quality.</p>
+                    <p>I like to see myself as a passionate developer, and an eternal learner.</p>
 
-                <div className='cubespinner'>
-                    <div className='face1'>
-                        <FontAwesomeIcon icon={faReact} color="#DD0031" />
-                    </div>
-                    <div className='face2'>
-                        <FontAwesomeIcon icon={faHtml5} color="#F06529" />
-                    </div>
-                    <div className='face3'>
-                        <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
-                    </div>
-                    <div className='face4'>
-                        <FontAwesomeIcon icon={faJsSquare} color="#5ED4F4" />
-                    </div>
-                    <div className='face5'>
-                        <FontAwesomeIcon icon={faNodeJs} color="#EFD81D" />
-                    </div>
-                    <div className='face6'>
-                        <FontAwesomeIcon icon={faGithub} color="#EC4E28" />
+                </div>
+                <div className='stage-cube-cont'>
+
+                    <div className='cubespinner'>
+                        <div className='face1'>
+                            <FontAwesomeIcon icon={faReact} color="#DD0031" />
+                        </div>
+                        <div className='face2'>
+                            <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+                        </div>
+                        <div className='face3'>
+                            <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+                        </div>
+                        <div className='face4'>
+                            <FontAwesomeIcon icon={faJsSquare} color="#5ED4F4" />
+                        </div>
+                        <div className='face5'>
+                            <FontAwesomeIcon icon={faNodeJs} color="#EFD81D" />
+                        </div>
+                        <div className='face6'>
+                            <FontAwesomeIcon icon={faGithub} color="#EC4E28" />
+                        </div>
                     </div>
                 </div>
-            </div>
 
-        </div>
+            </div>
+            <Loader type='pacman'/>
+        </>
     )
 }
 
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,43 +4,48 @@ import React, { useEffect, useState } from 'react'
 import { Link } from "react-router-dom"
 import AnimatedLetters from '../AnimatedLetters/index'
 import Logo from "./Logo/index"
+import Loader from "react-loaders"
 const Home = () => {
-    const [letterClass,setLetterClass] = useState("text-animate")
+    const [letterClass, setLetterClass] = useState("text-animate")
     const nameArray = "mandloi".split("")
     const jobArray = "web developer.".split("")
 
-    useEffect(()=>{
-         setTimeout(()=>{
-         return   setLetterClass("text-animate-hover")
-        },4000)
-    },[])
+    useEffect(() => {
+        setTimeout(() => {
+            return setLetterClass("text-animate-hover")
+        }, 4000)
+    }, [])
     return (
-        <div className="container home-page">
-            <div className="text-zone">
-                <h1>
-                    <span className={letterClass}>H</span>
-                    <span className={`${letterClass} _12`}>i,</span>
-                    <br /> 
-                    <span className={`${letterClass} _13`}>I</span>
-                    <span className={`${letterClass} _14`}>'m</span>
-                    <img src={LogoTitle} alt="developer" />
-                    <AnimatedLetters letterClass={letterClass}
-                    strArray={nameArray}
-                    idx={15}
-                    />
-                    <br/>
-                    <AnimatedLetters letterClass={letterClass}
-                    strArray={jobArray}
-                    idx={22}
-                    />
-                    <br />
-                    web developer
+        <>
+
+            <div className="container home-page">
+                <div className="text-zone">
+                    <h1>
+                        <span className={letterClass}>H</span>
+                        <span className={`${letterClass} _12`}>i,</span>
+                        <br />
+                        <span className={`${letterClass} _13`}>I</span>
+                        <span className={`${letterClass} _14`}>'m</span>
+                        <img src={LogoTitle} alt="developer" />
+                        <AnimatedLetters letterClass={letterClass}
+                            strArray={nameArray}
+                            idx={15}
+                        />
+                        <br />
+                        <AnimatedLetters letterClass={letterClass}
+                            strArray={jobArray}
+                            idx={22}
+                        />
+                        <br />
+                        web developer
                     </h1>
                     <h2>Frontend Developer / JavaSript Developer / Flutter Developer</h2>
                     <Link to="/contact" className="flat-button">CONTACT ME</Link>
+                </div>
+                <Logo />
             </div>
-            <Logo/>
-        </div>
+            <Loader type='pacman' />
+        </>
     )
 }
 
